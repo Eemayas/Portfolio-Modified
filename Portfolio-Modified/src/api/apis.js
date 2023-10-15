7288523acc558a5e058591a73d1c099076613a7e
Prashant Manandhar <100434825+Eemayas@users.noreply.github.com>
2023-10-15 22:29:13 +0545

    Api url changed

--- Portfolio-Modified/src/api/apis.js
+++ Portfolio-Modified/src/api/apis.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:5000/portfoilo";
+// const url = "http://localhost:5000/portfoilo";
+const url = "https://profoilo-backend.onrender.com/portfoilo";
 
 export const fetchBio = () => axios.get(`${url}/bio`);
 export const postBio = (newBio) => axios.post(`${url}/bio`, newBio);
